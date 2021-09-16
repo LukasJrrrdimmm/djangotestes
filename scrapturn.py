641b6466422e428b065098cdcc1ca0e00f19a7c6
Lukas Maximo Grilo Abreu Jardim <lukasmaximo@gmail.com>
2021-09-16 16:04:10 -0300

    Add files via upload

--- scrapturn.py
+++ scrapturn.py
@@ -20,5 +20,26 @@ scrapped_data = {
 
 print(json.dumps(scrapped_data, indent=4, ensure_ascii=False))
 aux = scraptime.strftime('%d-%m-%Y %H-%M')
-with open(f"json/output({aux}).json", 'w') as outfile:
+with open(f"json/outputg1({aux}).json", 'w') as outfile:
+    json.dump(scrapped_data, outfile, indent=4, ensure_ascii=False)
+    
+data = rqs.get('https://ge.globo.com/')
+scraptime = datetime.now()
+soup = bsp(data.text, 'html.parser')
+
+scrapped_data = {
+        'full_content': [span.text for span in soup.find_all('div', {'class':'feed-post-body'})],
+        'headers':[span.text for span in soup.find_all('div', {'class': 'feed-post-header with-post-chapeu'})],
+        'title':[span.text for span in soup.find_all('div', {'class': 'feed-post-body-title '
+                                                                         'gui-color-primary gui-color-hover'})],
+        'review':[span.text for span in soup.find_all('div', {'class': 'feed-post-body-resumo'})],
+        'related':[span.text for span in soup.find_all('div', {'class': 'bstn-fd-relatedtext'})],
+        'feed_time':[span.text for span in soup.find_all('div', {'class': 'feed-post-metadata'})],
+        'scraptime': scraptime.strftime('%d/%m/%Y %H:%M')
+
+    }
+
+print(json.dumps(scrapped_data, indent=4, ensure_ascii=False))
+aux = scraptime.strftime('%d-%m-%Y %H-%M')
+with open(f"json/outputge({aux}).json", 'w') as outfile:
     json.dump(scrapped_data, outfile, indent=4, ensure_ascii=False)
