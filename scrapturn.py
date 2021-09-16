13bd343280c49ae1245c82e3357c98b018107e7a
Lukas Maximo Grilo Abreu Jardim <lukasmaximo@gmail.com>
2021-09-16 15:09:22 -0300

    Add files via upload

--- scrapturn.py
+++ scrapturn.py
@@ -1,18 +1,24 @@
 import requests as rqs
 from bs4 import BeautifulSoup as bsp
+import json
+from datetime import datetime
 
 data = rqs.get('https://g1.globo.com/')
+scraptime = datetime.now()
 soup = bsp(data.text, 'html.parser')
 
-import numpy as np
-import tensorflow as tf
-headers = [span.text for span in soup.find_all('div', {'class': 'feed-post-header with-post-chapeu'})]
-title = [span.text for span in soup.find_all('div', {'class': 'feed-post-body-title gui-color-primary gui-color-hover'})]
-review = [span.text for span in soup.find_all('div', {'class': 'feed-post-body-resumo'})]
+scrapped_data = {
+        'full_content': [span.text for span in soup.find_all('div', {'class':'feed-post-body'})],
+        'headers':[span.text for span in soup.find_all('div', {'class': 'feed-post-header with-post-chapeu'})],
+        'title':[span.text for span in soup.find_all('div', {'class': 'feed-post-body-title '
+                                                                         'gui-color-primary gui-color-hover'})],
+        'review':[span.text for span in soup.find_all('div', {'class': 'feed-post-body-resumo'})],
+        'related':[span.text for span in soup.find_all('div', {'class': 'bstn-fd-relatedtext'})],
+        'feed_time':[span.text for span in soup.find_all('div', {'class': 'feed-post-metadata'})],
+        'scraptime': scraptime.strftime('%d/%m/%Y %H:%M')
+    }
 
-print("Headers")
-print(headers)
-print("Title")
-print(title)
-print("Review")
-print(review)
+print(json.dumps(scrapped_data, indent=4, ensure_ascii=False))
+aux = scraptime.strftime('%d-%m-%Y %H-%M')
+with open(f"json/output({aux}).json", 'w') as outfile:
+    json.dump(scrapped_data, outfile, indent=4, ensure_ascii=False)
